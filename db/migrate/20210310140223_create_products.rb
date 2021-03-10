d5039b67b14ea3461100d344b4dfcb9e26bb555e
Diego Perez <diegopr178@gmail.com>
2021-03-10 12:30:33 -0300

    experimento scaffold

---
+++ db/migrate/20210310140223_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.integer :quant
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Product.destroy_all
+
+Category.create(name: 'hogar')
+
+5.times do
+
+    Product.create(
+        [
+            {
+                name: Faker::House.furniture,
+                price: rand(10000..50000), 
+                quant: rand(1..100)
+            }
+        ]
+    )
+
+    Category.last.products << Product.last
+
+end
+
+Category.create(name: 'computacion')
+
+5.times do
+
+    Product.create(
+        [
+            {
+                name: Faker::Computer.type,
+                price: rand(10000..50000),
+                quant: rand(1..100)
+            }
+        ]
+    )
+
+    Category.last.products << Product.last
+    Category.first.products << Product.last
+
+end
+
+Category.create(name: 'libros')
+
+5.times do
+
+    Product.create(
+        [
+            {
+                name: Faker::Book.title,
+                price: rand(10000..50000),
+                quant: rand(1..100)
+            }
+        ]
+    )
+
+    Category.last.products << Product.last
+
+end
