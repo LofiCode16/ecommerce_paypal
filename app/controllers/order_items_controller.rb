f0557ebc837de88b692379ded7bb6d60be719888
Diego perez <diegopr178@gmail.com>
2021-03-14 21:01:04 -0300

    ADD: fill cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,23 @@
 class OrderItemsController < ApplicationController
 
-    def show
-        @order_items = Order.find_by(user_id: current_user.id, payed: false).order_items
+    def index
+        @order = Order.find_by(user_id: current_user.id, payed: false)
+        @order_items = @order.order_items unless @order.nil?
     end
+
+    def create
+        @order = Order.find_or_create_by(user_id: current_user.id, payed: false)
+        @product = Product.find(params[:product_id])
+        @order_item = OrderItem.find_or_create_by(order_id: @order.id, product_id: @product.id)
+
+        if @order_item.quantity.nil?
+            @order_item.update(quantity: 1, total: @product.price)
+        else
+            @order_item.update(quantity: @order_item.quantity + 1, total: @product.price * (@order_item.quantity + 1))
+        end
+
+        redirect_to root_path, notice: 'producto añadido al carro'
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :products
 
   get 'cart', to: 'order_items#index', as: 'cart'
+  post 'cart/:product_id/add', to: 'order_items#create', as: 'cart_add'
 
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
